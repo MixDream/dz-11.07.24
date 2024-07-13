d10c0f962d81872a0a9219f8411b30d671002f7f
MixDream <max_juldashev@mail.ru>
2024-07-13 21:53:54 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,7 @@
 def test_function():
-    return
-    def inner_function():
-      print("Я в области видимости функции test_function")
-      return
+    inner_function()
+def inner_function():
+    print("Я в области видимости функции test_function")
 
 test_function()
 inner_function()
